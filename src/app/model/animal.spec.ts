858f51c7100f51e62bd2ee25bb4ab322c01f4fd7
Joe Keegan <joe@jjk3.com>
2024-09-30 18:00:14 -0400

    prettier

--- src/app/model/animal.spec.ts
+++ src/app/model/animal.spec.ts
@@ -1,59 +1,60 @@
 import { Animal } from './animal';
-import {Player} from "./player.model";
+import { Player } from './player.model';
 
-const testPlayer : Player = {
+const testPlayer: Player = {
   id: 'test',
-  name: 'NimbusScale'
-}
+  name: 'NimbusScale',
+};
 
 describe('Animal', () => {
   it('should create an instance', () => {
     expect(new Animal(testPlayer, 'mammalAnimal')).toBeTruthy();
   });
   it('should set a name based on kind', () => {
-    const animal = new Animal(testPlayer, 'mammalAnimal')
-    expect(animal.name).toEqual('Mammal')
-  })
+    const animal = new Animal(testPlayer, 'mammalAnimal');
+    expect(animal.name).toEqual('Mammal');
+  });
   it('non-amphibian should have two inherent elements', () => {
-    const animal = new Animal(testPlayer, 'mammalAnimal')
-    expect(animal.elements.length).toEqual(2)
-    expect(animal.elements[0].kind).toEqual('meatElement')
-  })
+    const animal = new Animal(testPlayer, 'mammalAnimal');
+    expect(animal.elements.length).toEqual(2);
+    expect(animal.elements[0].kind).toEqual('meatElement');
+  });
   it('amphibian should have three inherent elements', () => {
-    const animal = new Animal(testPlayer, 'amphibianAnimal')
-    expect(animal.elements.length).toEqual(3)
-    expect(animal.elements[0].kind).toEqual('waterElement')
-  })
+    const animal = new Animal(testPlayer, 'amphibianAnimal');
+    expect(animal.elements.length).toEqual(3);
+    expect(animal.elements[0].kind).toEqual('waterElement');
+  });
   describe('addElement', () => {
     it('add an element if animal has 6 or less elements', () => {
-      const animal = new Animal(testPlayer, 'amphibianAnimal')
-      const currentElementCount = animal.elements.length
-      animal.addElement({kind: 'waterElement'})
-      expect(animal.elements.length).toEqual(currentElementCount + 1)
-    })
+      const animal = new Animal(testPlayer, 'amphibianAnimal');
+      const currentElementCount = animal.elements.length;
+      animal.addElement({ kind: 'waterElement' });
+      expect(animal.elements.length).toEqual(currentElementCount + 1);
+    });
     it('Errors if animal has more than 6 elements', () => {
-      const animal = new Animal(testPlayer, 'amphibianAnimal')
-      const currentElementCount = animal.elements.length
-      for (let i = currentElementCount; i <= 6 ; i++) {
-        animal.addElement({kind: 'waterElement'})
+      const animal = new Animal(testPlayer, 'amphibianAnimal');
+      const currentElementCount = animal.elements.length;
+      for (let i = currentElementCount; i <= 6; i++) {
+        animal.addElement({ kind: 'waterElement' });
       }
-      expect(() => {animal.addElement({kind: 'waterElement'})}).toThrowError()
-    })
-  })
+      expect(() => {
+        animal.addElement({ kind: 'waterElement' });
+      }).toThrowError();
+    });
+  });
   describe('removeElement', () => {
     it('removes an element if that kind of element is an addedElements', () => {
-      const animal = new Animal(testPlayer, 'amphibianAnimal')
-      const currentElements = animal.elements.map(element => element.kind)
-      animal.addElement({kind: 'meatElement'})
-      animal.removeElement({kind: 'meatElement'})
-      expect(animal.elements.map(element => element.kind)).toEqual(currentElements)
-
-    })
+      const animal = new Animal(testPlayer, 'amphibianAnimal');
+      const currentElements = animal.elements.map((element) => element.kind);
+      animal.addElement({ kind: 'meatElement' });
+      animal.removeElement({ kind: 'meatElement' });
+      expect(animal.elements.map((element) => element.kind)).toEqual(currentElements);
+    });
     it('Errors if try to remove an element that is not an addedElements', () => {
-      const animal = new Animal(testPlayer, 'amphibianAnimal')
-      expect(() => {animal.removeElement({kind: 'waterElement'})}).toThrowError()
-    })
-  })
-
-
+      const animal = new Animal(testPlayer, 'amphibianAnimal');
+      expect(() => {
+        animal.removeElement({ kind: 'waterElement' });
+      }).toThrowError();
+    });
+  });
 });
--- src/app/model/animal.ts
+++ src/app/model/animal.ts
@@ -1,5 +1,5 @@
-import {Player} from "./player.model";
-import {Element, ElementKind} from "./element.model";
+import { Player } from './player.model';
+import { Element, ElementKind } from './element.model';
 
 export type AnimalKind =
   | 'amphibianAnimal'
@@ -15,30 +15,30 @@ export const inherentElementKindByAnimalKind = new Map<AnimalKind, ElementKind>(
   ['birdAnimal', 'seedElement'],
   ['insectAnimal', 'grassElement'],
   ['mammalAnimal', 'meatElement'],
-  ['reptileAnimal', 'sunElement']
-] as const)
+  ['reptileAnimal', 'sunElement'],
+] as const);
 
 export class Animal {
-  readonly owner: Player
-  readonly kind: AnimalKind
-  private readonly inherentElements: Element[] = []
-  private readonly addedElements: Element[] = []
+  readonly owner: Player;
+  readonly kind: AnimalKind;
+  private readonly inherentElements: Element[] = [];
+  private readonly addedElements: Element[] = [];
 
   constructor(owner: Player, kind: AnimalKind) {
-    this.owner = owner
-    this.kind = kind
-    this.initialize()
+    this.owner = owner;
+    this.kind = kind;
+    this.initialize();
   }
 
   private initialize() {
-    const inherentElementKind = inherentElementKindByAnimalKind.get(this.kind)
+    const inherentElementKind = inherentElementKindByAnimalKind.get(this.kind);
     if (inherentElementKind) {
-      const inherentElementCount = this.kind === 'amphibianAnimal' ? 3 : 2
+      const inherentElementCount = this.kind === 'amphibianAnimal' ? 3 : 2;
       for (let i = 0; i < inherentElementCount; i++) {
-        this.inherentElements.push({kind: inherentElementKind})
+        this.inherentElements.push({ kind: inherentElementKind });
       }
     } else {
-      throw new Error(`Unable to find inherentElementKind for Animal type ${this.kind}`)
+      throw new Error(`Unable to find inherentElementKind for Animal type ${this.kind}`);
     }
   }
 
@@ -48,24 +48,27 @@ export class Animal {
   }
 
   get elements(): Element[] {
-    return [...this.inherentElements, ...this.addedElements]
+    return [...this.inherentElements, ...this.addedElements];
   }
 
   addElement(element: Element): void {
     if (this.elements.length <= 6) {
-      this.addedElements.push(element)
+      this.addedElements.push(element);
     } else {
-      throw new Error(`${this.name} already has 6 Elements.`)
+      throw new Error(`${this.name} already has 6 Elements.`);
     }
   }
 
   removeElement(element: Element): void {
-    const elementIndex = this.addedElements.findIndex(addedElement => addedElement.kind === element.kind)
+    const elementIndex = this.addedElements.findIndex(
+      (addedElement) => addedElement.kind === element.kind,
+    );
     if (elementIndex !== -1) {
-      this.addedElements.splice(elementIndex, 1)
+      this.addedElements.splice(elementIndex, 1);
     } else {
-      throw new Error(`${this.name} does not have element kind ${element.kind} as an added element.`)
+      throw new Error(
+        `${this.name} does not have element kind ${element.kind} as an added element.`,
+      );
     }
   }
-
 }
