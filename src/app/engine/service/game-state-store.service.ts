40c58c3a1368598bf883937a7fe07caf523cc783
Joe Keegan <joe@jjk3.com>
2024-10-03 14:35:28 -0400

    transaction checkpoint

--- src/app/engine/service/game-state-store.service.ts
+++ src/app/engine/service/game-state-store.service.ts
@@ -3,12 +3,14 @@ import {BehaviorSubject, distinctUntilChanged, map, Observable} from 'rxjs';
 import {FactionState} from '../model/faction.model';
 import {GameState, GameStateElement} from '../model/game-state.model';
 import {Injectable} from "@angular/core";
+import { deepClone } from 'fast-json-patch';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GameStateStoreService {
   private gameState: GameState;
+  private transactionState: GameState | null = null
   private gameStateSubject: BehaviorSubject<GameState>;
 
   constructor(gameState: GameState) {
@@ -33,8 +35,11 @@ export class GameStateStoreService {
     );
   }
 
-  private setGameStateElement(key: keyof GameState, newState: GameStateElement): void {
-    const subStateArray = this.gameState[key] as (typeof newState)[];
+  private setTransactionStateElement(key: keyof GameState, newState: GameStateElement): void {
+    if (!this.transactionState) {
+      throw new Error('Must start transaction before updating state.')
+    }
+    const subStateArray = this.transactionState[key] as (typeof newState)[];
     const index = subStateArray.findIndex((item) => item.kind === newState.kind);
 
     if (index > -1) {
@@ -44,26 +49,54 @@ export class GameStateStoreService {
     }
   }
 
-  // Will be replaced by function that will update state via GSP/
-  setState(newState: GameState) {
+  setGameState(newState: GameState) {
     this.gameState = newState;
     this.gameStateSubject.next(this.gameState);
   }
 
-  factionState$(): Observable<FactionState[]> {
+  startTransaction(): void {
+    if (!this.transactionState) {
+      this.transactionState = deepClone(this.gameState)
+    } else {
+      throw new Error("Can't start transaction and one already in progress.")
+    }
+
+  }
+
+  commitTransaction(): void {
+    if (this.transactionState) {
+      this.gameState = this.transactionState
+      this.transactionState = null
+      this.gameStateSubject.next(this.gameState);
+    } else {
+      throw new Error('No transaction in progress to commit')
+    }
+  }
+
+  rollbackTransaction(): void {
+    if (this.transactionState) {
+      this.transactionState = null
+      // Ensures gameState is a new object so that subscribers can detect the update
+      this.gameState = deepClone(this.gameState)
+      this.gameStateSubject.next(this.gameState);
+    } else {
+      throw new Error('No transaction in progress to rollback')
+    }
+  }
+
+  faction$(): Observable<FactionState[]> {
     return this.getObservableForKey((gameState) => gameState.faction);
   }
 
-  pileState$(): Observable<PileState<string>[]> {
+  setFaction(newState: FactionState): void {
+    this.setTransactionStateElement('faction', newState)
+  }
+
+  pile$(): Observable<PileState<string>[]> {
     return this.getObservableForKey((gameState) => gameState.pile);
   }
 
-  setPileState(newState: PileState<string>): void {
-    const index = this.gameState.pile.findIndex((item) => item.kind === newState.kind);
-    if (index > -1) {
-      this.gameState.pile[index] = newState;
-    } else {
-      this.gameState.pile.push(newState);
-    }
+  setPile(newState: PileState<string>): void {
+    this.setTransactionStateElement('pile', newState)
   }
 }
--- src/app/engine/service/game-state-store.spec.ts
+++ src/app/engine/service/game-state-store.spec.ts
@@ -1,9 +1,10 @@
 import { GameStateStoreService } from './game-state-store.service';
-import { skip } from 'rxjs';
+import { filter, skip } from 'rxjs';
 import {GameState} from "../model/game-state.model";
 
 describe('GameStateStore', () => {
   let testGameState: GameState;
+  let newGameState: GameState
   let gameStateStore: GameStateStoreService;
 
   beforeEach(() => {
@@ -35,41 +36,74 @@ describe('GameStateStore', () => {
         },
       ],
     };
+    newGameState = JSON.parse(JSON.stringify(testGameState))
+    newGameState.pile[0].inventory['test1'] = 20
     gameStateStore = new GameStateStoreService(testGameState);
   });
 
-  describe('pile', () => {
-    describe('pileState$', () => {
-      it('should have initial state', (done) => {
-        gameStateStore.pileState$().subscribe((newPileState) => {
-          expect(newPileState).toBe(testGameState.pile);
-          done();
-        });
-      });
-      it('should emit updated state when pile is updated', (done) => {
-        const newGameState = {
-          ...testGameState,
-          pile: [
-            {
-              kind: 'pile1',
-              inventory: { test1: 10, test2: 10 },
-            },
-            {
-              kind: 'pile2',
-              inventory: { test3: 20 },
-            },
-          ],
-        };
-        gameStateStore
-          .pileState$()
-          .pipe(skip(1)) // Skip the initial state
-          .subscribe((emittedPileState) => {
-            expect(emittedPileState).toBe(newGameState.pile);
-            done();
-          });
-
-        gameStateStore.setState(newGameState);
+  describe('state observable', () => {
+    it('$faction should emit state', (done) => {
+      gameStateStore.faction$().subscribe((emittedState) => {
+        expect(emittedState).toBe(testGameState.faction);
+        done();
       });
     });
-  });
+    it('$pile should emit state', (done) => {
+      gameStateStore.pile$().subscribe((emittedState) => {
+        expect(emittedState).toBe(testGameState.pile);
+        done();
+      });
+    });
+  })
+  describe('transaction', () => {
+    it('emits update state when commit', (done) => {
+      gameStateStore
+        .pile$()
+        .pipe(skip(1)) // Skip the initial state
+        .subscribe((emittedState) => {
+          expect(emittedState[0]).toEqual(newGameState.pile[0]);
+          done();
+        });
+
+      gameStateStore.startTransaction()
+      gameStateStore.setPile(newGameState.pile[0])
+      gameStateStore.commitTransaction()
+    })
+    it('emits original state when rollback', (done) => {
+      gameStateStore
+        .pile$()
+        .pipe(skip(1)) // Skip the initial state
+        .subscribe((emittedState) => {
+          expect(emittedState[0]).toEqual(testGameState.pile[0]);
+          done();
+        });
+
+      gameStateStore.startTransaction()
+      gameStateStore.setPile(newGameState.pile[0])
+      gameStateStore.rollbackTransaction()
+    })
+    it('throws error if update without transaction', () => {
+      expect(() => {
+        gameStateStore.setPile(newGameState.pile[0])
+      }).toThrowError()
+    })
+    it('throws error if start transaction, when one is already started', () => {
+      gameStateStore.startTransaction()
+      expect(() => {
+        gameStateStore.startTransaction()
+      }).toThrowError()
+    })
+    it('throws error if commit without transaction', () => {
+      expect(() => {
+        gameStateStore.commitTransaction()
+      }).toThrowError()
+    })
+    it('throws error if rollback without transaction', () => {
+      expect(() => {
+        gameStateStore.rollbackTransaction()
+      }).toThrowError()
+    })
+  })
+
+
 });
