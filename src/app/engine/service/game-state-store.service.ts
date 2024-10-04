f666f6a5e5058f17e25a1ff374f46e917d259009
Joe Keegan <joe@jjk3.com>
2024-10-04 15:21:12 -0400

    fixed bug where reference to GameState was being sent back to Store

--- src/app/engine/service/game-state-store.service.ts
+++ src/app/engine/service/game-state-store.service.ts
@@ -77,7 +77,7 @@ export class GameStateStoreService {
 
   private _setGameState(newState: GameState) {
     this._gameState = newState;
-    this.gameStateSubject.next(this._gameState);
+    this.gameStateSubject.next(this.gameState);
   }
 
   initializeGameState(gameState: GameState): void {
@@ -102,7 +102,7 @@ export class GameStateStoreService {
     if (!this._transactionState) {
       this._transactionState = deepClone(this._gameState) as GameState;
     } else {
-      throw new Error("Can't start transaction and one already in progress.");
+      throw new Error("Can't start transaction as one already in progress.");
     }
   }
 
