ceb77651b113a0bf2f37aa8471462341c2f377c3
Joe Keegan <joe@jjk3.com>
2024-10-06 17:01:17 -0400

    removed todo

--- src/app/engine/model/piece.model.ts
+++ src/app/engine/model/piece.model.ts
@@ -2,7 +2,6 @@ export interface Piece {
   kind: string;
 }
 
-// Todo: Should be an interface? /
 export type PieceFactory = (kind: string) => Piece;
 
 export function defaultPieceFactory(kind: string): Piece {
