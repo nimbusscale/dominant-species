fa0039c82e0dfade81b6129e6079cff9020210b0
Joe Keegan <joe@jjk3.com>
2024-10-15 13:51:40 -0400

    Working ElementSpace

--- src/app/game/component/action-display/element-space/element-space.component.ts
+++ src/app/game/component/action-display/element-space/element-space.component.ts
@@ -2,11 +2,16 @@ import {Component, Input} from '@angular/core';
 import {ElementPiece} from "../../../model/element.model";
 import {getOrThrow} from "../../../../engine/util";
 import {imgPathByKind} from "../../../constant/image.constant";
+import {ElementComponent} from "../../element/element.component";
+import {defaultPieceFactory} from "../../../../engine/model/piece.model";
+import {ElementEnum} from "../../../constant/element.constant";
 
 @Component({
   selector: 'app-element-space',
   standalone: true,
-  imports: [],
+  imports: [
+    ElementComponent
+  ],
   templateUrl: './element-space.component.html',
   styleUrl: './element-space.component.scss'
 })
