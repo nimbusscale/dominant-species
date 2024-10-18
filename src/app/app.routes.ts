b208f58754a199a800ea391f6da26a2f1d0e182b
Joe Keegan <joe@jjk3.com>
2024-10-18 16:07:35 -0400

    Initial working signup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {SignUpComponent} from "./engine/component/sign-up/sign-up.component";
+import {DrawPoolGameComponent} from "./game/component/draw-pool-game/draw-pool-game.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'game', component: DrawPoolGameComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: '', redirectTo: '/game', pathMatch: 'full' },
+  { path: '**', redirectTo: '/game' }
+];
