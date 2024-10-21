1398097da69b8421b9c128ed09b63bf5959a8b52
Joe K. <joe@jjk3.com>
2024-10-21 16:59:24 -0400

    vpaGameTable (#56)

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -3,6 +3,7 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { WebsiteBucket } from '../lib/website-bucket';
 import {UserPool} from "../lib/user-pool";
+import {VpaGamesTable} from "../lib/vpa-game-table";
 
 const stackProps = {
   env: {account: '011528296709', region: 'us-east-2'}
@@ -11,3 +12,4 @@ const stackProps = {
 const app = new cdk.App();
 new WebsiteBucket(app, 'WebsiteBucket', stackProps);
 new UserPool(app, 'UserPool', stackProps);
+new VpaGamesTable(app, 'vpaGameTable', stackProps);
