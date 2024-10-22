e73e24a52ff08b36ed1461a311b6a546f73c01e6
Joe Keegan <joe@jjk3.com>
2024-10-22 10:44:05 -0400

    with role

--- infra/lib/game-mgmt.ts
+++ infra/lib/game-mgmt.ts
@@ -1,17 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
 import {Construct} from 'constructs';
-import {aws_lambda, aws_lambda_nodejs} from "aws-cdk-lib";
+import {aws_lambda, aws_lambda_nodejs, aws_iam} from "aws-cdk-lib";
 import * as path from "node:path";
 
 export class GameMgmt extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const gameMgmtRole = new aws_iam.Role(this, 'GameMgmtRole', {
+      assumedBy: new aws_iam.ServicePrincipal('lambda.amazonaws.com')
+    })
+    gameMgmtRole.addManagedPolicy(aws_iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'))
+
+    const addUserToTableFromSignUpFunction = new aws_lambda_nodejs.NodejsFunction(this, 'addUserToTableFromSignUp', {
+      runtime: aws_lambda.Runtime.NODEJS_20_X,
+      entry: path.join(__dirname, '../backend/src/index.ts'),
+      handler: 'logEvent',
+      role: gameMgmtRole
+    })
   }
-
-  addUser = new aws_lambda_nodejs.NodejsFunction(this, 'vpaAddUserToTable', {
-    runtime: aws_lambda.Runtime.NODEJS_20_X,
-    entry: path.join(__dirname, '../backend/src/index.ts'),
-    handler: 'logEvent'
-  })
-
 }
