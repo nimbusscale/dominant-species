d072515b5096f44e6f1349fd197847b004e3d2f8
Joe Keegan <joe@jjk3.com>
2024-10-23 17:46:21 -0400

    consolidated package.json

--- packages/backend/src/lib/db/client.ts
+++ packages/backend/src/lib/db/client.ts
@@ -2,4 +2,5 @@ import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 
 const dynamoDBClient = new DynamoDBClient();
-export const dynamoDBDocumentClient = DynamoDBDocumentClient.from(dynamoDBClient);
+export const dynamoDBDocumentClient =
+  DynamoDBDocumentClient.from(dynamoDBClient);
---
+++ packages/infra/src/infra.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { WebsiteBucketStack } from './lib/website-bucket-stack';
+import { UserPoolStack } from './lib/user-pool-stack';
+import { VpaGamesTableStack } from './lib/vpa-game-table';
+import { GameMgmtStack } from './lib/game-mgmt-stack';
+
+const stackProps = {
+  env: { account: '011528296709', region: 'us-east-2' },
+};
+
+const app = new cdk.App();
+const vpaWebsiteBucket = new WebsiteBucketStack(app, 'vpaWebsiteBucket', stackProps);
+const vpaGameTable = new VpaGamesTableStack(app, 'vpaGameTable', stackProps);
+const vpaGameMgmt = new GameMgmtStack(app, 'vpaGameMgmt', stackProps, vpaGameTable.table);
+const vpaUserPool = new UserPoolStack(
+  app,
+  'vpaUserPool',
+  stackProps,
+  vpaGameMgmt.addUserToTableFromSignUpFunction,
+);
