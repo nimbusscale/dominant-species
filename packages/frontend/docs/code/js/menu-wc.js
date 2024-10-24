ad1826bc2b74be5028bd1b68b9841fd973398f69
nimbusscale <9387643+nimbusscale@users.noreply.github.com>
2024-10-24 15:10:50 +0000

    code-docs-update

--- packages/frontend/docs/code/js/menu-wc.js
+++ packages/frontend/docs/code/js/menu-wc.js
@@ -15,7 +15,7 @@ customElements.define('compodoc-menu', class extends HTMLElement {
         <nav>
             <ul class="list">
                 <li class="title">
-                    <a href="index.html" data-type="index-link">dominant-species documentation</a>
+                    <a href="index.html" data-type="index-link">frontend documentation</a>
                 </li>
 
                 <li class="divider"></li>
@@ -24,18 +24,8 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                     <a data-type="chapter-link" href="index.html"><span class="icon ion-ios-home"></span>Getting started</a>
                     <ul class="links">
                         <li class="link">
-                            <a href="overview.html" data-type="chapter-link">
-                                <span class="icon ion-ios-keypad"></span>Overview
-                            </a>
-                        </li>
-                        <li class="link">
                             <a href="index.html" data-type="chapter-link">
-                                <span class="icon ion-ios-paper"></span>README
-                            </a>
-                        </li>
-                        <li class="link">
-                            <a href="license.html"  data-type="chapter-link">
-                                <span class="icon ion-ios-paper"></span>LICENSE
+                                <span class="icon ion-ios-keypad"></span>Overview
                             </a>
                         </li>
                                 <li class="link">
