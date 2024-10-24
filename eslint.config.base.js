6331a9df16e5cb07e867d0da59798b48db8600bd
Joe Keegan <joe@jjk3.com>
2024-10-24 09:48:52 -0400

    frontend lint

---
+++ eslint.config.base.js
@@ -0,0 +1,27 @@
+// @ts-check
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+
+module.exports = tseslint.config(
+  {
+    files: ["src/**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
+    {
+    files: ["src/test/**/*.ts"],
+    rules: {
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/unbound-method": "off"
+    },
+  },
+);
