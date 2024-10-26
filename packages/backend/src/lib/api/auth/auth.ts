ae91ddd8702c40755946a0b3abe67aad41e83b40
Joe Keegan <joe@jjk3.com>
2024-10-26 09:44:41 -0400

    test fix try1

--- packages/backend/src/lib/api/auth/auth.ts
+++ packages/backend/src/lib/api/auth/auth.ts
@@ -1,7 +1,6 @@
 import { jwtDecode } from 'jwt-decode';
 import { CognitoIdTokenJwt } from 'api-types/src/auth';
-import {ensureDefined} from "../../util";
-
+import { ensureDefined } from '../../util';
 
 export function getUsernameFromIdToken(token: string): string {
   const decodedToken: CognitoIdTokenJwt = jwtDecode(token);
