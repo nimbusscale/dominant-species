ff048dd70e7124df3aa6e6fceadc03903893e921
Joe Keegan <joe@jjk3.com>
2024-10-27 16:19:03 -0400

    authInterceptor

--- packages/frontend/src/app/app.config.ts
+++ packages/frontend/src/app/app.config.ts
@@ -1,12 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {routes} from './app.routes';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
+import {authInterceptor} from "./engine/interceptor/auth";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     provideAnimationsAsync('noop'),
   ],
