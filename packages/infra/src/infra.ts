76f3903fe87ca57e4398fc82b8ec55e93798f53c
Joe Keegan <joe@jjk3.com>
2024-10-29 11:05:12 -0400

    stateMgmtBucket

--- packages/infra/src/infra.ts
+++ packages/infra/src/infra.ts
@@ -6,6 +6,7 @@ import { UserPoolStack } from './lib/user-pool-stack';
 import { VpaGamesTableStack } from './lib/vpa-game-table-stack';
 import { GameMgmtStack } from './lib/game-mgmt-stack';
 import { IamStack } from './lib/iam-stack';
+import {StateBucketStack} from "./lib/state-bucket-stack";
 
 const stackProps = {
   env: { account: '011528296709', region: 'us-east-2' },
@@ -13,7 +14,8 @@ const stackProps = {
 
 const app = new cdk.App();
 new WebsiteBucketStack(app, 'vpaWebsiteBucket', stackProps);
-const iamStack = new IamStack(app, 'vpaIam', stackProps);
+const stateBucketStack = new StateBucketStack(app, 'vpaStateBucket', stackProps)
+const iamStack = new IamStack(app, 'vpaIam', stackProps, stateBucketStack.bucket);
 const vpaGameTable = new VpaGamesTableStack(app, 'vpaGameTable', stackProps, iamStack.gameMgmtRole);
 const vpaUserPool = new UserPoolStack(
   app,
