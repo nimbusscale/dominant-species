80795870abcb3179c2c2ce02a20e53f05a142997
Joe Keegan <joe@jjk3.com>
2024-10-30 15:31:04 -0400

    set and get gamestate

--- packages/frontend/src/app/engine/service/game-management/game-management-client.service.ts
+++ packages/frontend/src/app/engine/service/game-management/game-management-client.service.ts
@@ -4,6 +4,7 @@ import { AuthService } from '../auth/auth.service';
 import { Game, GameCollection } from 'api-types/src/game';
 import { lastValueFrom } from 'rxjs';
 import { Player, PlayerCollection } from 'api-types/src/player';
+import {GameState} from "api-types/src/game-state";
 
 @Injectable({
   providedIn: 'root',
@@ -29,6 +30,14 @@ export class GameManagementClientService {
 
   async completeGame(gameId: string): Promise<void> {
     await lastValueFrom(this.http.patch(`/game/${gameId}`, { complete: true }));
+  }
+
+  async setInitialGameState(gameState: GameState): Promise<void> {
+    await lastValueFrom(this.http.post(`/game/${gameState.gameId}/state`, gameState))
+  }
+
+  async getLatestGameState(gameId: string): Promise<GameState> {
+    return await lastValueFrom(this.http.get<GameState>(`/game/${gameId}/state`))
   }
 
   async getPlayer(username: string): Promise<Player> {
