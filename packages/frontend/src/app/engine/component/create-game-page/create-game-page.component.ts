e753932ba7e2e9e28ac630ce3090e55e3729c907
Joe Keegan <joe@jjk3.com>
2024-10-31 15:39:28 -0400

    display error when invalid user

--- packages/frontend/src/app/engine/component/create-game-page/create-game-page.component.ts
+++ packages/frontend/src/app/engine/component/create-game-page/create-game-page.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { PlayerService } from "../../service/game-management/player.service";
-import { Player } from 'api-types/src/player';
-import { GameService } from "../../service/game-management/game.service";
-import { MatCard } from "@angular/material/card";
-import { MatFormField, MatFormFieldModule } from "@angular/material/form-field";
-import { FormArray, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { MatAutocomplete, MatAutocompleteTrigger, MatOption } from "@angular/material/autocomplete";
+import {Component, OnInit} from '@angular/core';
+import {PlayerService} from "../../service/game-management/player.service";
+import {Player} from 'api-types/src/player';
+import {GameService} from "../../service/game-management/game.service";
+import {MatCard} from "@angular/material/card";
+import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
+import {FormArray, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatAutocomplete, MatAutocompleteTrigger, MatOption} from "@angular/material/autocomplete";
 import {MatButton, MatIconButton} from "@angular/material/button";
-import { MatInput } from "@angular/material/input";
-import { NgForOf, NgIf } from "@angular/common";
-import { filter } from "rxjs";
-import { isNotUndefined } from "../../util/predicate";
-import { Router } from "@angular/router";
+import {MatInput} from "@angular/material/input";
+import {NgForOf, NgIf} from "@angular/common";
+import {filter} from "rxjs";
+import {isNotUndefined} from "../../util/predicate";
+import {Router} from "@angular/router";
 import {MatIcon} from "@angular/material/icon";
 
 @Component({
@@ -41,6 +41,7 @@ export class CreateGamePageComponent implements OnInit {
   form: FormGroup;
   filteredPlayers: string[][] = [[], [], [], [], []];
   errorMessages: string[] = ['', '', '', '', ''];
+  validPlayers: Set<string>[] = Array.from({length: 5}, () => new Set<string>());
 
   constructor(
     private fb: FormBuilder,
@@ -66,7 +67,9 @@ export class CreateGamePageComponent implements OnInit {
   }
 
   async onPlayerInput(index: number): Promise<void> {
-    const input = this.playerControls.at(index).value as string;
+    const control = this.playerControls.at(index);
+    const input = control.value as string;
+    this.validPlayers[index].clear();  // Reset valid players for this input
 
     const selectedPlayers = new Set(
       this.playerControls.value
@@ -74,15 +77,26 @@ export class CreateGamePageComponent implements OnInit {
     );
 
     this.filteredPlayers[index] = [];
+    this.errorMessages[index] = '';  // Reset the error message
+    control.setErrors(null);  // Clear any previous errors
 
     if (input) {
       try {
         const players = await this.playerService.findPlayers(input);
         this.filteredPlayers[index] = players.filter(player => !selectedPlayers.has(player));
-        this.errorMessages[index] = '';
+
+        // Populate validPlayers for validation
+        players.forEach(player => this.validPlayers[index].add(player));
+
+        // If input is not in valid players, show an error
+        if (!this.validPlayers[index].has(input)) {
+          this.errorMessages[index] = 'Invalid username';
+          control.setErrors({invalid: true});  // Mark control as invalid
+        }
       } catch (error) {
         this.errorMessages[index] = 'Error fetching players';
         console.error(error);
+        control.setErrors({fetchError: true});  // Mark control as invalid
       }
     }
   }
