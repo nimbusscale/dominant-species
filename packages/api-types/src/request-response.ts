7e5b0ba9ac659da35f2093c0d8d199dfca9d2fb2
Joe Keegan <joe@jjk3.com>
2024-11-09 10:15:20 -0500

    format

--- packages/api-types/src/request-response.ts
+++ packages/api-types/src/request-response.ts
@@ -1,6 +1,6 @@
-import {Game, GameCollection} from './game';
-import {Player, PlayerCollection} from './player';
-import {GameState} from './game-state';
+import { Game, GameCollection } from './game';
+import { Player, PlayerCollection } from './player';
+import { GameState } from './game-state';
 
 export type ApiResponseType = Game | GameCollection | GameState | Player | PlayerCollection;
 
