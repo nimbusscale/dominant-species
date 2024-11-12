ba40739aeaf7da7a7a5b35f94e31af7e6b0b9c00
Joe Keegan <joe@jjk3.com>
2024-11-12 10:39:43 -0500

    docstrings

--- packages/frontend/src/app/engine/service/game-element/game-element-registry.service.ts
+++ packages/frontend/src/app/engine/service/game-element/game-element-registry.service.ts
@@ -5,6 +5,25 @@ import { GameElement } from '../../model/game-element.model';
 import { getOrThrow } from '../../util/misc';
 import { GameElementState } from 'api-types/src/game-state';
 
+/**
+ * Abstract base class that manages the registration and retrieval
+ * of `GameElement` instances.
+ *
+ * This service:
+ * - Keeps track of registered `GameElement` instances.
+ * - Provides methods to retrieve them by ID.
+ * - Notifies observers when the set of registered IDs changes.
+ *
+ * It depends on a `GameElementStateService` to handle synchronization
+ * of `GameElement` states with the central `GameStateService`.
+ *
+ * Subclasses should provide specific implementations for different
+ * types of `GameElement`.
+ *
+ * @typeParam TgameElementState - The specific type of `GameElementState` managed by the subclass.
+ * @typeParam TgameElement - The specific type of `GameElement` managed by the subclass.
+ * @typeParam TgameElementStateSvc - The specific `GameElementStateService` used for state synchronization.
+ */
 @Injectable({
   providedIn: 'root',
 })
@@ -16,14 +35,33 @@ export abstract class GameElementRegistryService<
   private registeredIds: Set<string> = new Set<string>();
   private elementById: Map<string, TgameElement> = new Map<string, TgameElement>();
   private registeredElementSubject = new BehaviorSubject<Set<string>>(new Set());
+
   registeredIds$: Observable<Set<string>> = this.registeredElementSubject.asObservable();
 
   protected constructor(protected gameElementStateSvc: TgameElementStateSvc) {}
 
+  /**
+   * Retrieves a registered `GameElement` by its ID.
+   *
+   * @param id - The ID of the `GameElement` to retrieve.
+   * @returns The `GameElement` associated with the given ID.
+   * @throws If no `GameElement` with the given ID is registered.
+   */
   get(id: string): TgameElement {
     return getOrThrow(this.elementById, id);
   }
 
+  /**
+   * Registers one or more `GameElement` instances with the service.
+   *
+   * This method:
+   * - Adds the `GameElement` instances to the internal registry.
+   * - Emits the updated set of registered IDs.
+   * - Registers the elements with the `GameElementStateService` for state synchronization.
+   *
+   * @param elements - The `GameElement` or array of `GameElement` instances to register.
+   * @throws If a `GameElement` with the same ID is already registered.
+   */
   register(elements: TgameElement | TgameElement[]): void {
     const elementsArray = Array.isArray(elements) ? elements : [elements];
 
