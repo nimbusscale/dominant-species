f48607a41386a7ad14320c7cf8f2f8e98f3934ba
Joe Keegan <joe@jjk3.com>
2024-11-26 16:00:31 -0500

    Updated to take Space as args instead of the space pieces (i.e. either ActionPawn or null)

--- packages/frontend/src/app/game/component/action-display/space/action-pawn-space/action-pawn-space.component.ts
+++ packages/frontend/src/app/game/component/action-display/space/action-pawn-space/action-pawn-space.component.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { ActionPawnPiece } from '../../../../model/action-pawn.model';
+import {ChangeDetectionStrategy, Component, computed, input} from '@angular/core';
 import { ElementComponent } from '../../../element/element.component';
 import { ActionPawnComponent } from '../../../action-pawn/action-pawn.component';
 import { EyeballComponent } from '../eyeball/eyeball.component';
+import {Space} from "../../../../../engine/model/space.model";
+import {ActionPawnPiece} from "../../../../model/action-pawn.model";
 
 @Component({
   selector: 'app-action-pawn-space',
@@ -13,5 +14,27 @@ import { EyeballComponent } from '../eyeball/eyeball.component';
   styleUrl: './action-pawn-space.component.scss',
 })
 export class ActionPawnSpaceComponent {
-  @Input() actionPawn: ActionPawnPiece | null = null;
+  space = input.required<Space>()
+  actionPawn = computed(() => {
+    if (this.space().piece) {
+      return this.space().piece as ActionPawnPiece
+    } else {
+      /**
+       * Ideally we would return None, but '@if (actionPawn())' doesn't "narrow" the returned type in the template, so we have to return
+       * a compatible type for ActionPawnComponent, which in this case is undefined.
+       */
+      return undefined
+    }
+  })
+  hasAction = computed(() => {
+    return this.space().actions.length > 0;
+  })
+
+  performAction(): void {
+    if (this.space().actions.length > 0) {
+      this.space().actions[0].execute()
+    } else {
+      throw new Error("No action to perform")
+    }
+  }
 }
