935acb4fe820b4d2f8b5eadc6b529990c71678f6
Joe Keegan <joe@jjk3.com>
2024-11-27 16:53:57 -0500

    working area tests

--- packages/frontend/src/test/engine/model/area.model.spec.ts
+++ packages/frontend/src/test/engine/model/area.model.spec.ts
@@ -3,6 +3,7 @@ import { Space } from '../../../app/engine/model/space.model';
 import { skip } from 'rxjs';
 import { defaultPieceFactory } from '../../../app/engine/model/piece.model';
 import { Piece } from 'api-types/src/game-state';
+import {Action} from "../../../app/engine/model/action.model";
 
 describe('Area', () => {
   let area: Area;
@@ -75,6 +76,15 @@ describe('Area', () => {
       }).toThrowError();
     });
   });
+  describe('state$', () => {
+    it('should emit when a space state is updated', (done) => {
+      area.state$.pipe(skip(1)).subscribe((state) => {
+        expect(state.space[0].piece).toEqual(testPiece1);
+        done();
+      });
+      space1.addPiece(testPiece1);
+    })
+  })
   describe('spaces$', () => {
     it('Spaces should be emitted when space updated', (done) => {
       area.spaces$.pipe(skip(1)).subscribe((spaces) => {
@@ -90,7 +100,7 @@ describe('Area', () => {
         if (callCount === 1) {
           expect(spaces.map((space) => space.piece)).toEqual([testPiece1, null]);
         } else {
-          expect(spaces.map((space) => space.piece)).toEqual([testPiece1, testPiece1]);
+          expect(spaces.map((space) => space.piece)).toEqual([testPiece1, testPiece2]);
           done();
         }
       });
@@ -98,7 +108,7 @@ describe('Area', () => {
         id: 'testArea',
         space: [
           { kind: 'testSpace', piece: testPiece1 },
-          { kind: 'testSpace', piece: testPiece1 },
+          { kind: 'testSpace', piece: testPiece2 },
         ],
       };
       area.setState(newState);
@@ -123,4 +133,13 @@ describe('Area', () => {
       expect(area.nextAvailableSpace('otherKind')).toBeNull();
     });
   });
+  describe('clearActions', () => {
+    it('clears Actions', () => {
+      space1.setActions([jasmine.createSpyObj<Action>('Action', ['execute'])])
+      space2.setActions([jasmine.createSpyObj<Action>('Action', ['execute'])])
+      area.clearActions()
+      expect(space1.actions.length).toEqual(0)
+      expect(space2.actions.length).toEqual(0)
+    })
+  })
 });
